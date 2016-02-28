f3feab2d7fae83f6a25f61273678648151f8499f
markstewart <markstewy@gmail.com>
2016-02-28 14:18:00 -0700

    reset to random background image, still need to set img tint

--- mainCtrl.js
+++ mainCtrl.js
@@ -2,10 +2,10 @@ angular.module("welcomeApp").controller("welcomeCtrl", function($scope, $interva
     $scope.name = "test";
     var imgBank = [
 // /*ocean*/          "https://images.unsplash.com/photo-1449168013943-3a15804bb41c?crop=entropy&dpr=2&fit=crop&fm=jpg&h=725&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1425",
-// /*forest*/         "https://images.unsplash.com/photo-1448749927985-5565d99c10ae?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=b830c59c9205015b18f6c06889257bf1",
-// /*sf*/             "https://images.unsplash.com/photo-1452269826925-82be65baa057?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=813effa9de7316c73afe87f515173252",
+/*forest*/         "https://images.unsplash.com/photo-1448749927985-5565d99c10ae?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=b830c59c9205015b18f6c06889257bf1",
+/*sf*/             "https://images.unsplash.com/photo-1452269826925-82be65baa057?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=813effa9de7316c73afe87f515173252",
 /*cpark*/          "https://images.unsplash.com/photo-1452270313744-e5ccb6de0c78?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=ffd731f7388c6611aab2eba59720d47f",
-// /*fire*/           "https://images.unsplash.com/photo-1444210971048-6130cf0c46cf?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=16af70cd5a412898fbcd6e80940718d2",
+ /*fire*/           "https://images.unsplash.com/photo-1444210971048-6130cf0c46cf?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=16af70cd5a412898fbcd6e80940718d2",
 // /*camping*/        "https://images.unsplash.com/photo-1444124818704-4d89a495bbae?crop=entropy&dpr=2&fit=crop&fm=jpg&h=725&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1425",
 // /*hair*/           "https://images.unsplash.com/photo-1454909516657-78526f214d05?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=2368db70b62b646d4e7d1d030509cc84",
 // /*gf*/             "https://images.unsplash.com/uploads/1411070807173e4d6762d/f84a3a01?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=df3f701669d5ac5b7e2689e3690f8e7f",
@@ -13,22 +13,35 @@ angular.module("welcomeApp").controller("welcomeCtrl", function($scope, $interva
 // /*stadium*/        "https://images.unsplash.com/photo-1420177743490-15ee9ba8c78f?crop=entropy&dpr=2&fit=crop&fm=jpg&h=675&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1425"
                   ]
 
-    var i = 0;
-    $scope.imgMain = imgBank[i];
-    $interval(function () {
-      if (i === imgBank.length - 1) {
-        i = 0;
-        $scope.imgMain = imgBank[i];
-      }
-      else {
-        i++;
-        $scope.imgMain = imgBank[i];
-      }
-    }, 2000);
+    // ==========random image at start==========
+    // Returns a random integer between min (inclusive) and max (inclusive)
+    // Using Math.round() will give you a non-uniform distribution!
 
-    $timeout(function () {
-      $scope.menuSelect = true;
-    }, 2000);
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    var i = getRandomInt(0, 3);
+    console.log(i);
+    $scope.imgMain = imgBank[i];
+
+
+    // ====image cycle======
+    // var i = 0;
+    // $scope.imgMain = imgBank[i];
+    // $interval(function () {
+    //   if (i === imgBank.length - 1) {
+    //     i = 0;
+    //     $scope.imgMain = imgBank[i];
+    //   }
+    //   else {
+    //     i++;
+    //     $scope.imgMain = imgBank[i];
+    //   }
+    // }, 5000);
+
+    // $timeout(function () {
+    //   $scope.menuSelect = true;
+    // }, 2000);
 
     var launch = Date("June 30, 2016 08:00:00:000")
 
@@ -37,5 +50,8 @@ angular.module("welcomeApp").controller("welcomeCtrl", function($scope, $interva
       // $scope.dDiff = launch.getTime() - dateNow.getTime();
     }, 1000);
 
-
+//Look before you leap.
+//Because there's more to life than making student loan payments..
+//Make big deicsions with confidence.
+//
 });
